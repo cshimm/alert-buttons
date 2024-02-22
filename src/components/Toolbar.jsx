e1fee158c6f6e0edd579cbd6b3920ad6413878a6
cshimm <cameronjshimmin@gmail.com>
2024-02-21 20:20:25 -0800

    dynamic alert buttons

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,9 +1,16 @@
 import {AlertButton} from "./AlertButton";
 
 export const Toolbar = () => {
+    const buttonData = [
+        {message: '1!', children: 'Download File 1'},
+        {message: '2!', children: 'Download File 2'},
+        {message: '3!', children: 'Download File 3'},
+        {message: '4!', children: 'Download File 4'},
+        {message: '5!', children: 'Download File 5'},
+    ]
     return <>
-        <AlertButton children={"1"} message={"1!"}/>
-        <AlertButton children={"2"} message={"2!"}/>
-        <AlertButton children={"3"} message={"3!"}/>
+        {
+            buttonData.map(button => <AlertButton message={button.message} children={button.children}/>)
+        }
     </>
 }
