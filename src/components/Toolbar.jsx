ee8a9bac0e77b8540bb60f79c2132df3f34babbb
cshimm <cameronjshimmin@gmail.com>
2024-02-21 20:43:01 -0800

    dynamic alert buttons: added explanations

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -7,9 +7,14 @@ export const Toolbar = () => {
         {message: '3!', children: 'Download File 3'},
         {message: '4!', children: 'Download File 4'},
         {message: '5!', children: 'Download File 5'},
+        // Empty objects to test default values
+        {},
+        {message:"Empty children"},
+        {children: "Empty message"},
     ]
     return <>
         {
+            // Message and chidlren prop check for truthy values, if not a default value is passed in.
             buttonData.map(button => <AlertButton message={button.message || "Default message"} children={button.children || "Default children"}/>)
         }
     </>
